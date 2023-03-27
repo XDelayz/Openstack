a5e39a6c83b041766b2104dc0d3936e8cf6153a7
Stack_Voodoo <118162868+XDelayz@users.noreply.github.com>
2023-03-27 07:23:00 +0300

    Update deploy-instance.sh

--- deploy-instance.sh
+++ deploy-instance.sh
@@ -1,71 +1,51 @@
 #!/bin/bash
-echo "Deploying"
-#Random number generate
-NUM=$((1 + RANDOM % 200)) &&
-# Create port 
-DNIC=$(openstack network list | grep "#" | awk -F '|' '{print $2}' | awk '{print substr($0,2,37)}') &&
-DSUB=$(openstack subnet list | grep "#" | awk -F '|' '{print $2}' | awk '{print substr($0,2,37)}') &&
-DPORT=$(openstack port create --network "$DNIC" --fixed-ip subnet="$DSUB",ip-address="192.168.2.$NUM" PORT -f value -c id) &&
-#Image_menu 
-echo "Size_Volume"; read -r i ;
-PS3="Image:"
-select z in "Windows 10 RU" \
-"Windows_Server_2019_Standart_EN" \
-"Windows Server 2019 Standart RU" \
-"Windows2019-SysElegance" \
-"Windows2016-SysElegance" \
-"Windows_Server_2016_Standart_EN" \
-"Windows_Server_2016_Standart_RU" \
-"Windows_Server_2012R2_Standart_EN" \
-"Windows_Server_2012R2_Standart_RU" \
-"Ubuntu Server 16.04.7 LTS x64" \
-"Ubuntu Server 18.04.5 LTS x64" \
-"Ubuntu Server 20.04.4 LTS x64 (Focal Fossa)" \
-"Ubuntu Server 22.04 LTS x64 (Jammy Jellyfish)" \
-"Veeam Recovery Media 5.0.0 Windows" \
-"VeeamRecoveryMedia_4.0_Linux" \
-"CentOS 7.9.2009" \
-"CentOS_8.1.1911_x64" \
-"CentOS_Stream" \
-"Debian 10.6 x64" \
-"Debian 10.9 x64" \
-"Debian 9.13 x64" \
-"FreeBSD_11.3" \
-"FreeBSD_12.2_Release";
-do
+im=$(openstack image list -f value -c Name) # команда выводит список образов и записывает в переменную
+images=() # создается пустой массив
+while read -r line; do # построчное чтение списка образов из переменной "$im"
+images+=("$line")
+done <<< "$im"
+
+PS3="Image:" # выводит меню из образов для выбора
+select z in "${images[@]}"; do
 break
 done
+
 PS3="Availability-zone:"
 select zone in "lviv" \
 "nova";
 do
-#Create_Volume	
+echo "Size_Volume"; read -r i ; 
+
+# создается диск на основе образа для дальнейшего создания сервера с диска
 DVOL=$(openstack volume create --image "$z" --size "$i" --availability-zone "$zone" Volume -f yaml | xargs | awk '{print substr($0,161,36)}') &&
 break
 done
-#Flavor_menu
 echo "Name_Instance"; read -r b ;
+
+fla=$(openstack flavor list -f value -c Name)
+flavor=()
+while read -r line; do
+flavor+=("$line")
+done <<< "$fla"
+
 PS3="Flavor:"
-select flavor in "ext_1024_p1_50gb" \
-"ext_2048_p1_50gb" \
-"ext_4096_p2_50gb" \
-"ext_8192_p4_50gb" \
-"ext_12288_p4_50gb" \
-"ext_16384_p6_50gb" \
-"ext_24576_p8_50gb" \
-"ext_32768_p10_50gb" \
-"ext_40960_p12_50gb" \
-"ext_49152_p16_50gb" \
-"ext_65536_p18_50gb" \
-"ext_98304_p24_50gb";
-do
-#Create_Instance	
-openstack server create "$b" --flavor "$flavor" --volume "$DVOL" --availability-zone "$zone" --port "$DPORT"
+select z in "${flavor[@]}"; do
 break
 done
-#Port Forwarding
-echo "Стандартный_port"; read -r in ; \
-echo "Нестандартный_port"; read -r ex ; \
+
+NUM=$((1 + RANDOM % 200)) && # генерируется рандомное число из диапазона 1-200
+DNIC=$(openstack network list | grep "#" | awk -F '|' '{print $2}' | awk '{print substr($0,2,37)}') &&
+DSUB=$(openstack subnet list | grep "#" | awk -F '|' '{print $2}' | awk '{print substr($0,2,37)}') &&
+
+# создается порт
+DPORT=$(openstack port create --network "$DNIC" --fixed-ip subnet="$DSUB",ip-address="192.168.2.$NUM" PORT -f value -c id) &&
+
+# создается сервер
+openstack server create "$b" --flavor "$flavor" --volume "$DVOL" --availability-zone "$zone" --port "$DPORT"
+
+# запрос на порт-форвардинг
+echo "internal_port"; read -r in ; \
+echo "external_port"; read -r ex ; \
 DFLO=$(openstack floating ip list | grep 185 | awk -F '|' '{print $2}' | awk '{print substr($1,0)}') &&
 echo "$ex" '>' ""192.168.2.$NUM"":"$in" > description.txt &&
 openstack floating ip port forwarding create --internal-ip-address "192.168.2.$NUM" --port "$DPORT" --internal-protocol-port "$in" \
