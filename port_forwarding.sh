b9e2168df66d3ed8ae917c0227faa4aeee0ae8bb
Stack_Voodoo <118162868+XDelayz@users.noreply.github.com>
2023-05-22 17:51:26 +0300

    Update port_forwarding.sh

--- port_forwarding.sh
+++ port_forwarding.sh
@@ -6,11 +6,16 @@ echo "external_port"; read -r ex ; \
 FLO=$(openstack floating ip list -f value -c "Floating IP Address") &&
 echo "$ex" '>' "$ip":"$in" > description.txt &&
 openstack port list | grep "$ip" | awk -F '|' '{print $2}' | awk '{print substr($1,0)}' > IPP.txt &&
-openstack floating ip port forwarding create --internal-ip-address "$ip" \
---port "$(< IPP.txt)" --internal-protocol-port "$in" \
---external-protocol-port "$ex" --protocol tcp "$FLO" &&
-openstack floating ip set "$FLO" --description "$(< description.txt)" &&
+openstack floating ip port forwarding create \
+--internal-ip-address "$ip" \
+--port "$(< IPP.txt)" \
+--internal-protocol-port "$in" \
+--external-protocol-port "$ex" \
+--protocol tcp "$FLO" &&
+openstack floating ip set "$FLO" \
+--description "$(< description.txt)" &&
 echo "Success port forwarding"
+
 
 <<COMMENT1
 Скрипт делает проброс порта, предварительно запрашивает ip-сервера, интернал-порт и экстернал-порт.
